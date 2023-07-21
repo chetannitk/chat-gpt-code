b9fbd57e0dd970a72e42ed13d7cc1f889633fbe6
chetan chauhan <chetanknmiet@gmail.com>
2023-07-21 00:02:01 -0500

    updated main function

--- nlp/extract-common-ngrams-sklearn.py
+++ nlp/extract-common-ngrams-sklearn.py
@@ -23,16 +23,19 @@ def extract_common_ngrams(docs, ngram_range=(1, 2), min_df=2, max_features=10):
     df.sort_values(by='frequency', ascending=False, inplace=True)
     return df.reset_index(drop=True)
 
+
 # Example usage:
-documents = [
-    "This is the first document. It contains some text.",
-    "The second document is here. It has different content.",
-    "And finally, the third document with more text."
-]
+if __name__ == "__main__":
+    # Example usage:
+    documents = [
+        "This is the first document. It contains some text.",
+        "The second document is here. It has different content.",
+        "And finally, the third document with more text."
+    ]
 
-ngram_range = (2, 3)  # Change this to extract different n-gram ranges (e.g., (1, 1) for unigrams, (2, 2) for bigrams, etc.)
-min_df = 1  # Minimum number of documents a token must appear in to be included
-max_features = 100  # Maximum number of features (n-grams) to return
+    ngram_range = (2, 3)  # Change this to extract different n-gram ranges (e.g., (1, 1) for unigrams, (2, 2) for bigrams, etc.)
+    min_df = 1  # Minimum number of documents a token must appear in to be included
+    max_features = 100  # Maximum number of features (n-grams) to return
 
-result_df = extract_common_ngrams(documents, ngram_range=ngram_range, min_df=min_df, max_features=max_features)
-print(result_df)
+    result_df = extract_common_ngrams(documents, ngram_range=ngram_range, min_df=min_df, max_features=max_features)
+    print(result_df)
